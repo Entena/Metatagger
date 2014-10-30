0f5ede83ea481b4da4960108a894f14a0dc340bd
Tim Eck II <pulsence@gmail.com>
2014-10-30 17:46:08 -0400

    -Added select by meta data functionality
    -the ' char is now properly escaped when building sql statements
    -the sql script templates where updated to include ' when enclosing a text
    value
    -a test was added to test adding a song with an ' character
    
    -added functionality to get songs given a list of ids
    
    -refactored the DBTest class. Each type of test is called in it's own
    function now
    
    -defined the beginnings of the plugin interface

---
+++ src/gui/plugin/FinishedSongStatus.java
@@ -0,0 +1,12 @@
+package gui.plugin;
+
+/**
+ * This enum defines the different finish statuses that a song could have
+ * finished in
+ * @author pulsence
+ *
+ */
+public enum FinishedSongStatus {
+    COMPLETED,
+    SKIPPED
+}
