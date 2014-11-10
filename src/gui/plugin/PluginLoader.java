b0c89b0c19fd5d4a6b40bc260cafd873d0129e17
Tim Eck II <pulsence@gmail.com>
2014-11-10 10:25:07 -0500

    -created untested plugin loading code -started work on ui intergration
    -added a .gitignore file
    -plugin code is now "intergrated" but fairly untested
        -still need to finish pop up dialog that shows loaded plugins
    
    -I hate the temp folder which keeps appearing

---
+++ src/gui/plugin/PluginLoader.java
@@ -0,0 +1,35 @@
+package gui.plugin;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class PluginLoader {
+    
+    @SuppressWarnings("unchecked")
+    public ArrayList<LearningPlugin> loadPlugin(String filePath) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException{
+        ArrayList<LearningPlugin> plugins = new ArrayList<LearningPlugin>();
+        
+        URLClassLoader loader = new URLClassLoader(new URL[] {new URL(filePath)});
+        JarFile jarFile = new JarFile(filePath);
+        Enumeration<JarEntry> allEntries = jarFile.entries();
+        while (allEntries.hasMoreElements()) {
+            JarEntry entry = (JarEntry) allEntries.nextElement();
+            String name = entry.getName();
+            if(name.endsWith(".class")){
+                Class clazz = loader.loadClass(name.replace(".class", ""));
+                if(clazz.equals(LearningPlugin.class))
+                    plugins.add((LearningPlugin) clazz.newInstance());
+            }
+        }
+        
+        loader.close();
+        jarFile.close();
+        
+        return plugins;
+    }
+}
